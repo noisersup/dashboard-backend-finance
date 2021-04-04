290070c62c58a2eae00d6750312c21d98cc31e5a
noisersup <patekiapl@gmail.com>
2021-04-04 19:32:55 +0200

    :card_file_box: Expenses table

--- database/database.go
+++ database/database.go
@@ -17,29 +17,45 @@ func ConnectToDatabase(user, password, dbName string) (*Database,error){
 	db, err := sql.Open("postgres",payload)
 	if err != nil {return nil,err}
 	
-	err = CreateExpensesTable(db)
+	err = CreateGroupsTable(db)
+	if err != nil {return nil,err}
 	
+	err = CreateExpensesTable(db)
 	if err != nil {return nil,err}
 	
 	return &Database{db},nil
 }
 
-func CreateExpensesTable(db *sql.DB) error{
-	_, err := db.Exec(`CREATE TABLE IF NOT EXISTS expenses (
-							id		SERIAL,
-							title	TEXT NOT NULL,
-							cost	NUMERIC(10,2) NOT NULL
+func CreateGroupsTable(db *sql.DB) error{
+	_, err := db.Exec(`CREATE TABLE IF NOT EXISTS groups (
+							id				SERIAL,
+							title			TEXT NOT NULL UNIQUE,
+							maxExpenses		NUMERIC(10,2) NOT NULL,
+							currExpenses	NUMERIC(10,2) NOT NULL,
+							PRIMARY KEY (id)
 						)`)
 	return err
 }
 
-func (db *Database) CreateExpense(title string, cost float64) (*models.Expense, error) {
+func CreateExpensesTable(db *sql.DB) error{
+	_, err := db.Exec(`CREATE TABLE IF NOT EXISTS expenses (
+							id			SERIAL,
+							group_id	int NOT NULL,
+							title		TEXT NOT NULL,
+							cost		NUMERIC(10,2) NOT NULL,
+							PRIMARY KEY (id),
+							FOREIGN KEY (group_id) REFERENCES groups(id) ON DELETE CASCADE
+						)`)
+	return err
+}
+
+func (db *Database) CreateExpense(title string,cost float64, group int) (*models.Expense, error) {
 	var expense models.Expense
 	expense.Title = title
 	expense.Cost = cost
 
-	err := db.db.QueryRow("INSERT INTO expenses(title, cost) VALUES ($1,$2) RETURNING ID",
-						  title,cost).Scan(&expense.Id)
+	err := db.db.QueryRow("INSERT INTO expenses(title, group_id, cost) VALUES ($1,$2,$3) RETURNING ID",
+						  title,group,cost).Scan(&expense.Id)
 	if err != nil {return nil,err}
 
 	return &expense,nil
