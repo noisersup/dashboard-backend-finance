219b4dcda27fa18dc727196bd93be3e488827ad2
noisersup <patekiapl@gmail.com>
2021-04-05 17:01:05 +0200

    :heavy_plus_sign: Added github.com/lib/pq

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/noisersup/dashboard-backend-finance
 
 go 1.16
 
-require gorm.io/gorm v1.21.6 // indirect
+require (
+	github.com/lib/pq v1.10.0 // indirect
+	gorm.io/gorm v1.21.6 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"flag"
+	"log"
+
+	"github.com/noisersup/dashboard-backend-finance/database"
+	u "github.com/noisersup/dashboard-backend-finance/utils"
+)
+
 func main(){
+	userPtr := flag.String("user","","Specify username to postgres database.")
+	passwdPtr := flag.String("passwd","","Specify passwd to postgres database.")
+	dbPtr := flag.String("database","","Specify name of postgres database.")
+	flag.Parse()
+	
+	if *userPtr==""||*passwdPtr==""||*dbPtr=="" { log.Fatalf("You must specify username, password and name of database!")}
+
+
+	db,err := database.ConnectToDatabase(*userPtr,*passwdPtr,*dbPtr)
+	if err != nil {u.Err("Databse Error",err)}
+
 	
 }
