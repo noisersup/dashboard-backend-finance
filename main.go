89a551526ccb6602428e49a4f92ce9eac34c0816
noisersup <patekiapl@gmail.com>
2021-04-06 13:02:17 +0200

    :wrench: added database configuration file

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
+	"io/ioutil"
 	"log"
+	"os"
 
 	"github.com/noisersup/dashboard-backend-finance/database"
 	u "github.com/noisersup/dashboard-backend-finance/utils"
 )
+
+type DbConfig struct{
+	username	string
+	password	string
+	database	string
+}
 
 func main(){
 	userPtr := flag.String("user","","Specify username to postgres database.")
@@ -14,11 +23,34 @@ func main(){
 	dbPtr := flag.String("database","","Specify name of postgres database.")
 	flag.Parse()
 	
-	if *userPtr==""||*passwdPtr==""||*dbPtr=="" { log.Fatalf("You must specify username, password and name of database!")}
-
-
-	db,err := database.ConnectToDatabase(*userPtr,*passwdPtr,*dbPtr)
-	if err != nil {u.Err("Databse Error",err)}
-
+	var config DbConfig
+	var err error
+	if *userPtr==""||*passwdPtr==""||*dbPtr=="" { 
+		log.Printf("Authentication flags not set, loading passes from config file")
+		config, err = loadConfig("config/database.json")
+		if err != nil {log.Fatal(u.Err("Config file error",err))}
+	}else{
+		config.database = *dbPtr
+		config.username = *userPtr
+		config.password = *passwdPtr
+	}
 	
+	_,err = database.ConnectToDatabase(config.username,config.password,config.database)
+	if err != nil {u.Err("Databse Error",err)}
+}
+
+func loadConfig(jsonFile string) (DbConfig, error){
+	file, err := os.Open(jsonFile)
+	if err != nil {return DbConfig{},err}
+	
+	defer file.Close()
+	
+	bytes, err := ioutil.ReadAll(file)
+	if err != nil {return DbConfig{},err}
+	
+	var config DbConfig
+
+	json.Unmarshal([]byte(bytes),&config)
+
+	return config, nil
 }
