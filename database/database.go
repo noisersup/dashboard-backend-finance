514c1c7474353a16209fd1685a833e110e7b8f11
noisersup <patekiapl@gmail.com>
2021-08-11 22:08:42 +0200

    :construction: Working on independence of service

--- database/database.go
+++ database/database.go
@@ -1,52 +1,119 @@
 package database
 
 import (
+	"context"
 	"database/sql"
 	"errors"
 	"fmt"
 	"log"
+	"time"
 
 	_ "github.com/lib/pq"
 	"github.com/noisersup/dashboard-backend-finance/models"
 )
 
-type Database struct{
-	db 	*sql.DB
+var lastErr string
+
+func LogErr(err error) {
+	if err.Error() != lastErr {
+		log.Print(err)
+		lastErr = err.Error()
+	}
 }
 
-func ConnectToDatabase(host string ,port int,user, password, dbName string) (*Database,error){
+type Database struct {
+	db *sql.DB
+}
+
+func ConnectToDatabase(host string, port int, user, password, dbName string, timeout time.Duration) (*Database, error) {
 	payload := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-	host, port,user,password,dbName)
-	
+		host, port, user, password, dbName)
+
 	log.Printf("Connecting to database")
-	db, err := sql.Open("postgres",payload)
-	if err != nil {return nil,err}
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	done := make(chan *sql.DB)
+
+	var db *sql.DB
+
+	go func() {
+		for {
+			db, err := sql.Open("postgres", payload)
+			if err != nil {
+				log.Print(err)
+			} else {
+				done <- db
+				return
+			}
+		}
+	}()
+
+	select {
+	case ndb := <-done:
+		db = ndb
+		cancel()
+		break
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	}
+
 	log.Printf("Connected to database")
 
 	log.Printf("Checking for groups table")
-	err = CreateGroupsTable(db)
-	if err != nil {return nil,err}
+	err := CreateGroupsTable(db, timeout)
+	if err != nil {
+		return nil, err
+	}
 
-	log.Printf("Checking for expenses table")	
-	err = CreateExpensesTable(db)
-	if err != nil {return nil,err}
-	
-	return &Database{db},nil
+	log.Printf("Checking for expenses table")
+	err = CreateExpensesTable(db, timeout)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Database{db}, nil
 }
 
-func CreateGroupsTable(db *sql.DB) error{
-	_, err := db.Exec(`CREATE TABLE IF NOT EXISTS groups (
+func CreateGroupsTable(db *sql.DB, timeout time.Duration) error {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+	done := make(chan bool)
+
+	go func() {
+		for {
+			_, err := db.Exec(`CREATE TABLE IF NOT EXISTS groups (
 							id				SERIAL,
 							title			TEXT NOT NULL UNIQUE,
 							max_expenses		NUMERIC(10,2) NOT NULL,
 							curr_expenses	NUMERIC(10,2) NOT NULL DEFAULT 0.00,
 							PRIMARY KEY (id)
 						)`)
-	return err
+			if err != nil {
+				LogErr(err)
+			} else {
+				done <- true
+				return
+			}
+		}
+	}()
+	select {
+	case <-done:
+		cancel()
+		return nil
+	case <-ctx.Done():
+		return ctx.Err()
+	}
 }
 
-func CreateExpensesTable(db *sql.DB) error{
-	_, err := db.Exec(`CREATE TABLE IF NOT EXISTS expenses (
+func CreateExpensesTable(db *sql.DB, timeout time.Duration) error {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+	done := make(chan bool)
+
+	go func() {
+		_, err := db.Exec(`CREATE TABLE IF NOT EXISTS expenses (
 							id			SERIAL,
 							group_id	int NOT NULL,
 							title		TEXT NOT NULL,
@@ -54,72 +121,114 @@ func CreateExpensesTable(db *sql.DB) error{
 							PRIMARY KEY (id),
 							FOREIGN KEY (group_id) REFERENCES groups(id) ON DELETE CASCADE
 						)`)
-	return err
+		if err != nil {
+			LogErr(err)
+		} else {
+			done <- true
+			return
+		}
+
+	}()
+	select {
+	case <-done:
+		cancel()
+		return nil
+	case <-ctx.Done():
+		return ctx.Err()
+	}
 }
 
-func (db *Database) CreateExpense(title string,cost float64, group int) (*models.Expense, error) {
-	if title == "" || cost <= 0 || group==0 {
-		return nil, errors.New("CreateGroup: title is empty, cost value is not valid or group is not specified.")
+func (db *Database) CreateExpense(title string, cost float64, group int, timeout time.Duration) (*models.Expense, error) {
+
+	if title == "" || cost <= 0 || group == 0 {
+		return nil, errors.New("CreateExpense: title is empty, cost value is not valid or group is not specified.")
 	}
 	var expense models.Expense
 	expense.Title = title
 	expense.Cost = cost
 
-	err := db.db.QueryRow("INSERT INTO expenses(title, group_id, cost) VALUES ($1,$2,$3) RETURNING ID",
-						  title,group,cost).Scan(&expense.Id)
-	if err != nil {return nil,err}
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+	done := make(chan bool)
 
-	return &expense,nil
+	go func() {
+		for {
+			err := db.db.QueryRow("INSERT INTO expenses(title, group_id, cost) VALUES ($1,$2,$3) RETURNING ID",
+				title, group, cost).Scan(&expense.Id)
+			if err != nil {
+				LogErr(err)
+			} else {
+				done <- true
+				return
+			}
+		}
+	}()
+
+	select {
+	case <-done:
+		cancel()
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	}
+	return &expense, nil
 }
 
-func (db *Database) CreateGroup(title string,maxExpenses float64) (*models.Group, error) {
+func (db *Database) CreateGroup(title string, maxExpenses float64) (*models.Group, error) {
 	if title == "" || maxExpenses <= 0 {
 		return nil, errors.New("CreateGroup: title is empty or max expenses value is not valid")
 	}
 	group := models.Group{
-		Title: title,
+		Title:       title,
 		MaxExpenses: maxExpenses,
 	}
 	err := db.db.QueryRow("INSERT INTO groups(title, max_expenses) VALUES ($1,$2) RETURNING ID",
-						  title,maxExpenses).Scan(&group.Id)
+		title, maxExpenses).Scan(&group.Id)
 
-	if err != nil {return nil,err}
+	if err != nil {
+		return nil, err
+	}
 
-	return &group,nil
+	return &group, nil
 }
 
-func (db *Database) GetGroups() ([]models.Group,error){
+func (db *Database) GetGroups() ([]models.Group, error) {
 	var groups []models.Group
-	
+
 	rows, err := db.db.Query("SELECT * from groups")
-	if err != nil {return nil,err}
+	if err != nil {
+		return nil, err
+	}
 
 	defer rows.Close()
-	
+
 	for rows.Next() {
 		var group models.Group
-		if err := rows.Scan(&group.Id,&group.Title,&group.MaxExpenses,&group.CurrExpenses) ; err != nil {
+		if err := rows.Scan(&group.Id, &group.Title, &group.MaxExpenses, &group.CurrExpenses); err != nil {
 			return nil, err
 		}
-		
-		group.Expenses,err = db.GetExpensesInGroup(group.Id)
-		if err != nil { return nil, err }
+
+		group.Expenses, err = db.GetExpensesInGroup(group.Id)
+		if err != nil {
+			return nil, err
+		}
 
 		groups = append(groups, group)
 	}
 	return groups, nil
 }
 
-func (db *Database) GetExpensesInGroup(id int) ([]models.Expense,error){
-	rows, err := db.db.Query("SELECT id,title,cost from expenses WHERE group_id = $1",id)
-	if err != nil {return nil,err}
+func (db *Database) GetExpensesInGroup(id int) ([]models.Expense, error) {
+	rows, err := db.db.Query("SELECT id,title,cost from expenses WHERE group_id = $1", id)
+	if err != nil {
+		return nil, err
+	}
 	defer rows.Close()
 
 	var expenses []models.Expense
 
 	for rows.Next() {
 		var expense models.Expense
-		if err := rows.Scan(&expense.Id,&expense.Title,&expense.Cost) ; err != nil {
+		if err := rows.Scan(&expense.Id, &expense.Title, &expense.Cost); err != nil {
 			return nil, err
 		}
 		expenses = append(expenses, expense)
@@ -128,13 +237,21 @@ func (db *Database) GetExpensesInGroup(id int) ([]models.Expense,error){
 }
 
 func (db *Database) ResetDatabase() error {
-	if _,err := db.db.Exec("DELETE FROM expenses") ; err != nil { return err}
-	if _,err := db.db.Exec("DELETE FROM groups") ; err != nil { return err}
-	if _,err := db.db.Exec("ALTER SEQUENCE expenses_id_seq RESTART WITH 1") ; err != nil { return err}
-	if _,err := db.db.Exec("ALTER SEQUENCE groups_id_seq RESTART WITH 1") ; err != nil { return err}
+	if _, err := db.db.Exec("DELETE FROM expenses"); err != nil {
+		return err
+	}
+	if _, err := db.db.Exec("DELETE FROM groups"); err != nil {
+		return err
+	}
+	if _, err := db.db.Exec("ALTER SEQUENCE expenses_id_seq RESTART WITH 1"); err != nil {
+		return err
+	}
+	if _, err := db.db.Exec("ALTER SEQUENCE groups_id_seq RESTART WITH 1"); err != nil {
+		return err
+	}
 	return nil
-} 
+}
 
-func (db *Database) CloseDatabase() error{
+func (db *Database) CloseDatabase() error {
 	return db.db.Close()
 }
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"net"
 	"os"
 	"strconv"
+	"time"
 
 	"github.com/noisersup/dashboard-backend-finance/api"
 	"github.com/noisersup/dashboard-backend-finance/api/pb"
@@ -16,44 +17,45 @@ import (
 	"google.golang.org/grpc"
 )
 
-type DbConfig struct{
-	Address		string `json:"address"`
-	Port		int    `json:"port"`
-	Username	string `json:"username"`
-	Password	string `json:"password"`
-	Database	string `json:"database"`
+type DbConfig struct {
+	Address  string `json:"address"`
+	Port     int    `json:"port"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Database string `json:"database"`
 }
 
-func main(){
+func main() {
 	config := getVars()
 
-	db,err := database.ConnectToDatabase(config.Address,config.Port,config.Username,config.Password,config.Database)
-	if err != nil {log.Fatal(u.Err("Database Error",err))}
+	db, err := database.ConnectToDatabase(config.Address, config.Port, config.Username, config.Password, config.Database, 2*time.Second)
+	if err != nil {
+		log.Fatal(u.Err("Database Error", err))
+	}
 
-	defer func ()  {
-		if err := db.CloseDatabase(); err != nil{
-			log.Fatal(u.Err("Closing database error",err))
+	defer func() {
+		if err := db.CloseDatabase(); err != nil {
+			log.Fatal(u.Err("Closing database error", err))
 		}
 	}()
 
-
-	lis, err := net.Listen("tcp",":9000")
+	lis, err := net.Listen("tcp", ":9000")
 	if err != nil {
-		log.Fatalf("Failed to listen on port 9000: %v",err)
+		log.Fatalf("Failed to listen on port 9000: %v", err)
 	}
 	log.Print(lis.Addr())
 
 	a := api.InitAPI(db)
 
 	server := grpc.NewServer()
-	pb.RegisterFinanceServiceServer(server,a)
+	pb.RegisterFinanceServiceServer(server, a)
 
 	if err := server.Serve(lis); err != nil {
-		log.Fatalf("Failed to serve: %v",err)
+		log.Fatalf("Failed to serve: %v", err)
 	}
 }
 
-func getVars() *DbConfig {	
+func getVars() *DbConfig {
 	var config DbConfig
 
 	config.Address = os.Getenv("DB_ADDRESS")
@@ -61,35 +63,41 @@ func getVars() *DbConfig {
 	config.Username = os.Getenv("DB_USERNAME")
 	config.Password = os.Getenv("DB_PASSWORD")
 
-	config.Port,_ = strconv.Atoi(os.Getenv("DB_PORT")) //default: 5432
+	config.Port, _ = strconv.Atoi(os.Getenv("DB_PORT")) //default: 5432
 
-	if config.Address=="" || config.Database==""||config.Username==""||config.Password==""{ 
+	if config.Address == "" || config.Database == "" || config.Username == "" || config.Password == "" {
 		log.Printf("ENV variables not set, loading config from config file")
-		
+
 		var err error
 		config, err = loadConfig("config/database.json")
-		if err != nil {log.Fatal(u.Err("Config file error",err))}
+		if err != nil {
+			log.Fatal(u.Err("Config file error", err))
+		}
 	}
 
 	if config.Port == 0 {
 		log.Print("Port invalid or not provided. Setting default (5432)")
-		config.Port=5432
+		config.Port = 5432
 	}
 	return &config
 }
 
-func loadConfig(jsonFile string) (DbConfig, error){
+func loadConfig(jsonFile string) (DbConfig, error) {
 	file, err := os.Open(jsonFile)
-	if err != nil {return DbConfig{},err}
-	
+	if err != nil {
+		return DbConfig{}, err
+	}
+
 	defer file.Close()
-	
+
 	bytes, err := ioutil.ReadAll(file)
-	if err != nil {return DbConfig{},err}
-	
+	if err != nil {
+		return DbConfig{}, err
+	}
+
 	var config DbConfig
 
-	json.Unmarshal([]byte(bytes),&config)
+	json.Unmarshal([]byte(bytes), &config)
 
 	return config, nil
 }
